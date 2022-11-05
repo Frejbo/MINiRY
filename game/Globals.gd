7eb9263e58418c59d4d08ec8740f260f113334a7
Frejbo <frej.hellsten@gmail.com>
2022-11-05 14:24:51 +0100

    constructor med items LETS GOOO

--- game/Globals.gd
+++ game/Globals.gd
@@ -1,1 +1,8 @@
 enum items {IronOre, IronIngot, IronGear, IronRod}
+const amount_of_total_items = 3
+const item_paths = {
+	items.IronOre: "res://items/IronOre.gltf",
+	items.IronIngot: "res://items/IronIngot.gltf",
+	items.IronGear: "res://items/IronGear.gltf",
+	items.IronRod: "res://items/IronRod.gltf",
+}
--- game/constructor/constructor.gd
+++ game/constructor/constructor.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 var anim = $AnimationPlayer
-const can_process = [Globals.items.IronIngot]
+var can_produce = [Globals.items.IronGear, Globals.items.IronRod]
+var producing = Globals.items.IronGear
 
 func _on_process_area_body_entered(body):
 	if not body.is_in_group("item"): return # only smelt items... :flushed:
@@ -19,11 +20,29 @@ func _on_process_area_body_entered(body):
 	anim.play_backwards("ArmatureAction")
 
 
+var currently_producing : int = 0
+func _process(_delta):
+	var item_node = $Item/item
+	if currently_producing == producing: return
+	# change item
+	item_node.get_child(0).queue_free()
+	
+	var item = load(Globals.item_paths[producing]).instantiate()
+	item_node.add_child(item)
+	print(producing)
+	currently_producing = producing
 
 func click_arrow(area):
 	if "Right" in area.name:
+		producing += 1
+		while not producing in can_produce:
+			producing += 1
+			if producing > Globals.amount_of_total_items: producing = 0 
 		$AnimationArrows.play("RightArrows")
-		
 	
 	if "Left" in area.name:
+		producing -= 1
+		while not producing in can_produce:
+			producing -= 1
+			if producing < 0: producing = Globals.amount_of_total_items
 		$AnimationArrows.play("LeftArrows")
