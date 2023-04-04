aed25c6167e25726fa50bd9729e8bb34d3063b7c
Frejbo <frej.hellsten@gmail.com>
2023-04-04 14:40:32 +0200

    see previous commit

--- game/Graphics.gd
+++ game/Graphics.gd
@@ -7,7 +7,12 @@ var PARTICLE_QUALITY = QUALITY_SCALE.medium
 
 
 func _notification(what):
-	if what == NOTIFICATION_EXIT_TREE:
-		# save settings
-		var file = FileAccess.open("user://settings.json", FileAccess.WRITE)
-		file.store_var(PARTICLE_QUALITY)
+	match what:
+		NOTIFICATION_EXIT_TREE:
+			# save settings
+			var file = FileAccess.open("user://settings.json", FileAccess.WRITE)
+			file.store_var(PARTICLE_QUALITY)
+		NOTIFICATION_ENTER_TREE:
+			if not FileAccess.file_exists("user://settings.json"): return # ingen save-fil hittas, ladda ej.
+			var file = FileAccess.open("user://settings.json", FileAccess.READ)
+			PARTICLE_QUALITY = file.get
